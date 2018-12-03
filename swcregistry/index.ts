d236acb29115894732dd68d902c2301903937581
ersul4ik <ersul4ik@mail.ru>
2018-12-03 11:29:58 +0600

    Removed SWCregistry class

--- swcregistry/index.ts
+++ swcregistry/index.ts
@@ -3,35 +3,6 @@ import { XMLHttpRequest } from 'xmlhttprequest-ts';
 
 const fs = require("fs");
 const fetch = require('node-fetch')
-const file_definition = require('./swc-definition.json');
-
-class SWCRegistry {
-    public update_file_content(content){
-        fs.writeFile("./swc-definition.json", JSON.stringify(content), (err) => {
-            if (err) {
-                console.error(err);
-                return;
-            }
-            return;
-        });
-    }
-
-    public get_latest_version(){
-        const url = 'https://raw.githubusercontent.com/SmartContractSecurity/SWC-registry/master/export/swc-definition.json';
-        fetch(url)
-            .then(res => res.json())
-            .then(res => {
-              this.update_file_content(res);
-              return;
-            })
-            .catch(res => {
-                throw new Error(res.statusText)
-        });
-    }
-    public get_content_by_file(){
-        return file_definition;
-    }
-}
 
 
 class SWC {
@@ -42,26 +13,47 @@ class SWC {
         swc = new SWC('SWC-100')
         console.log(swc.title)
     If you need a latest version of swc-definition.json file, use:
-        swc = new SWC('SWC-100')
-        swc.get_latest_version()
-        console.log(swc.title)
+        const swc = new SWC('SWC-100');
+        swc.update(err => {
+            if(err) {
+                console.log(err);
+            } else {
+                console.log(swc.title());
+            }
+        });
     */
     swc_id: string;
     constructor(swc_id) {
         this.swc_id = swc_id;
     }
 
-    public update(){
-        return new SWCRegistry().get_latest_version();
+    public static update_file_content(content, done){
+        fs.writeFile("./swc-definition.json", JSON.stringify(content), (err) => {
+            if (err) {
+                console.error(err);
+                done(err);
+                return;
+            }
+            done(null)
+            return;
+        });
+    }
+
+    public update(done){
+        const url = 'https://raw.githubusercontent.com/SmartContractSecurity/SWC-registry/master/export/swc-definition.json';
+        return fetch(url)
+            .then(res => res.json())
+            .then(res => SWC.update_file_content(res, done))
+            .catch(err => done(err));
     }
 
     public content(){
-        const entry = new SWCRegistry();
-        if (entry.get_content_by_file()[this.swc_id] == undefined){
+        const rawdata = JSON.parse(fs.readFileSync('./swc-definition.json'));  
+        if (rawdata[this.swc_id] == undefined){
             console.log(`SWC with ID ${this.swc_id} does not exist`);
             return {}
         }
-        return entry.get_content_by_file()[this.swc_id]['content'];
+        return rawdata[this.swc_id]['content'];
     }
 
     public title() {
