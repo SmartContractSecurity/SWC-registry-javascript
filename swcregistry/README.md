dc2a64f2fda7bd611a412d0ed797c9104738b3e7
ersul4ik <ersul4ik@mail.ru>
2019-01-04 03:49:13 +0000

    Changed the package version [skip ci]

---
+++ swcregistry/README.md
@@ -0,0 +1,37 @@
+# SWC-registry-javascript
+[![CircleCI](https://circleci.com/gh/SmartContractSecurity/SWC-registry-javascript.svg?style=svg)](https://circleci.com/gh/SmartContractSecurity/SWC-registry-javascript)
+
+JavaScript library for accessing SWC-registry content
+
+
+## Example
+```typescript
+const { SWC } = require('swc-registry');
+
+
+const swc = new SWC('SWC-100');
+console.log(swc.title());
+
+// Function Default Visibility
+```
+
+## Behaviour
+
+On first use of the SWC methods, the SWC registry is initialized from file (swc-definition.json) out cache. If user want to get a latest information of SWC-registry he will have to use the update() method
+
+### Get latest version
+```typescript
+const { SWC } = require('swc-registry');
+
+
+const swc = new SWC('SWC-100');
+
+swc.update(err => {
+if(err) {
+    console.log(err);
+} else {
+    console.log(swc.relationships());
+}});
+
+// [CWE-710: Improper Adherence to Coding Standards](https://cwe.mitre.org/data/definitions/710.html)
+```
--- swcregistry/src/index.ts
+++ swcregistry/src/index.ts
@@ -8,9 +8,8 @@ const rawdata = require('./swc-definition.json');
 
 const dirString = path.dirname(fs.realpathSync(__filename));
 
-
 class SWC {
-    /*
+  /*
     SWC class contains information on an SWC entry
 
     Example usage:
@@ -26,45 +25,46 @@ class SWC {
             }
         });
     */
-    private SWCID: string;
-    private rawdata: JSON;
-    constructor(SWCID) {
-        this.SWCID = SWCID;
-        this.rawdata = rawdata;
-    }
+  private SWCID: string;
+  private rawdata: JSON;
+  constructor(SWCID) {
+    this.SWCID = SWCID;
+    this.rawdata = rawdata;
+  }
 
-    public update_file_content(content, done){
-        this.rawdata = content;
-        fs.writeFile('swc-definition.json', JSON.stringify(content), (err) => {
-            if (err) {
-                console.error(err);
-                done(err);
-                return;
-            }
-            done(null)
-            return;
-        });
-    }
+  public update_file_content(content, done) {
+    this.rawdata = content;
+    fs.writeFile('swc-definition.json', JSON.stringify(content), err => {
+      if (err) {
+        console.error(err);
+        done(err);
+        return;
+      }
+      done(null);
+      return;
+    });
+  }
 
-    public update(done){
-        const url = 'https://raw.githubusercontent.com/SmartContractSecurity/SWC-registry/master/export/swc-definition.json';
-        return fetch(url)
-            .then(res => res.json())
-            .then(res => this.update_file_content(res, done))
-            .catch(err => done(err));
-    }
+  public update(done) {
+    const url =
+      'https://raw.githubusercontent.com/SmartContractSecurity/SWC-registry/master/export/swc-definition.json';
+    return fetch(url)
+      .then(res => res.json())
+      .then(res => this.update_file_content(res, done))
+      .catch(err => done(err));
+  }
 
-    public title() {
-        return this.rawdata[this.SWCID]['content']['Title'];
-    }
-    public relationships(){
-        return this.rawdata[this.SWCID]['content']['Relationships']
-    }
-    public description(){
-        return this.rawdata[this.SWCID]['content']['Description']
-    }
-    public remediation(){
-        return this.rawdata[this.SWCID]['content']['Remediation']
-    }
+  public title() {
+    return this.rawdata[this.SWCID]['content']['Title'];
+  }
+  public relationships() {
+    return this.rawdata[this.SWCID]['content']['Relationships'];
+  }
+  public description() {
+    return this.rawdata[this.SWCID]['content']['Description'];
+  }
+  public remediation() {
+    return this.rawdata[this.SWCID]['content']['Remediation'];
+  }
 }
 export { SWC };
